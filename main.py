050b1cf8a9af68681683198054f0f58b4aae1a48
Lam Nguyen Thai <nguyenthailam1909@gmail.com>
2026-07-11 11:46:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pickle
 import numpy as np
 
+
 # Khởi tạo API
 app = FastAPI(title="API Dự Đoán Sinh Viên")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Load mô hình (Đảm bảo file model.pkl nằm cùng thư mục)
 with open("model.pkl", "rb") as f:
